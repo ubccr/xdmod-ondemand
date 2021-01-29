209cef4ccb58bc2243d59a7eda2ffab8fb2d9951
Joseph White <jpwhite4@buffalo.edu>
2021-01-29 15:44:24 +0000

    Add setup script

---
+++ tests/scripts/setup.tcl
@@ -0,0 +1,29 @@
+#!/usr/bin/env expect
+# Configure a test ondemand resource for a new XDMoD instanct
+
+# Load helper functions
+source [file join [file dirname [info script]] ../../../xdmod/tests/ci/scripts/helper-functions.tcl]
+
+set timeout 240
+spawn "xdmod-setup"
+
+# Add an OnDemand resource
+selectMenuOption 4
+
+selectMenuOption 1
+provideInput {Resource Name:} styx
+provideInput {Formal Name:} {Open OnDemand Instance}
+provideInput {Resource Type*} Gateway
+provideInput {How many nodes does this resource have?} 0
+provideInput {How many total processors (cpu cores) does this resource have?} 0
+
+selectMenuOption s
+confirmFileWrite yes
+enterToContinue
+confirmFileWrite yes
+enterToContinue
+
+selectMenuOption q
+
+lassign [wait] pid spawnid os_error_flag value
+exit $value
