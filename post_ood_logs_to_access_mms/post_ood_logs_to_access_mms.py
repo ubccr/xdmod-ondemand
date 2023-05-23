87606b36ab0d4c5f0824fccd35b894f10031f6d5
Aaron Weeden <31246768+aaronweeden@users.noreply.github.com>
2023-05-23 09:36:54 -0400

    Add `apachelogs` module for parsing log files,
    - Use the last timestamp in the file to filter and sort log files
    instead of using last modified time.
    - Fix required Python version.
    - Rename some variables for clarity.

--- post_ood_logs_to_access_mms/post_ood_logs_to_access_mms.py
+++ post_ood_logs_to_access_mms/post_ood_logs_to_access_mms.py
@@ -5,8 +5,10 @@
 # will ingest and aggregate the data to be included in ACCESS XDMoD.
 
 # Import required libraries.
+from apachelogs import LogParser
 import argparse
 import configparser
+from datetime import datetime, timezone
 import glob
 import logging
 import os
@@ -18,38 +20,41 @@ with open('VERSION', 'r') as version_file:
     version = version_file.read()
 
 # Parse arguments.
-parser = argparse.ArgumentParser(
+arg_parser = argparse.ArgumentParser(
     description='Parse Open OnDemand logs and POST them to the ACCESS MMS team'
     + ' for inclusion in XDMoD.',
     allow_abbrev=False,
     formatter_class=argparse.ArgumentDefaultsHelpFormatter,
 )
-parser.add_argument(
+arg_parser.add_argument(
     '-c',
     '--conf-path',
     default='conf.ini',
     help='path to the configuration file that declares where to'
     + ' find log files and how to parse them',
 )
-parser.add_argument(
+arg_parser.add_argument(
     '-l',
     '--log-level',
     choices=('DEBUG', 'INFO', 'WARNING', 'ERROR'),
     default='WARNING',
 )
-parser.add_argument(
+arg_parser.add_argument(
     '-t',
     '--token-path',
     default='/root/.access_mms_ood_token',
     help='path to the file containing the API token obtained from the ACCESS'
     + ' MMS team',
 )
-parser.add_argument(
+arg_parser.add_argument(
     '--version',
     action='version',
     version=version,
 )
-args = parser.parse_args()
+args = arg_parser.parse_args()
+
+# Set datetime format.
+datetime_format = '%Y-%m-%d %H:%M:%S%Z'
 
 # Initialize the logger.
 logging.basicConfig()
@@ -57,10 +62,10 @@ logger = logging.getLogger(__file__)
 logger.setLevel(args.log_level)
 
 logger.info('Script starting.')
-logger.debug('Using arguments:' + str(args))
+logger.debug('Using arguments: ' + str(args))
 
 logger.debug('Initializing configuration file parser.')
-conf = configparser.ConfigParser(
+conf_parser = configparser.ConfigParser(
     # Empty values are OK and will be handled by this script.
     allow_no_value=True,
     # Make sure not to try to interpolate % signs in the log format.
@@ -68,58 +73,89 @@ conf = configparser.ConfigParser(
 )
 
 logger.debug("Parsing this script's configuration file.")
-parsed_filenames = conf.read(args.conf_path)
+parsed_filenames = conf_parser.read(args.conf_path)
 if not parsed_filenames:
     raise FileNotFoundError(args.conf_path + ' not found.')
 
 # Define a function to get a property from the configuration file.
-def __get_conf_property(conf, section, key):
+def __get_conf_property(conf_parser, section, key):
     logger.debug(
         'Getting ' + key + ' property from the [' + section
         + '] section of the configuration file:'
     )
-    value = conf.get(section, key)
+    value = conf_parser.get(section, key)
     logger.debug(value)
     return value
 
-log_dir = __get_conf_property(conf, 'logs', 'dir')
-log_format = __get_conf_property(conf, 'logs', 'format')
-filename_pattern = __get_conf_property(conf, 'logs', 'filename_pattern')
-last_run = __get_conf_property(conf, 'runs', 'last_run')
+# Get the properties from the configuration file.
+log_dir = __get_conf_property(conf_parser, 'logs', 'dir')
+log_format = __get_conf_property(conf_parser, 'logs', 'format')
+filename_pattern = __get_conf_property(conf_parser, 'logs', 'filename_pattern')
+before_time = __get_conf_property(conf_parser, 'runs', 'before_time')
 
-if last_run == '':
-    logger.debug('Last run is empty, setting to 0.0')
-    last_run = 0.0
+if before_time == '':
+    before_time = datetime.fromtimestamp(0, tz=timezone.utc)
+    logger.debug('before_time is empty, setting to ' + str(before_time))
+else:
+    before_time = datetime.strptime(before_time, datetime_format)
 
-logger.debug('Recording current time just before processing logs:')
-current_time = time.time()
-logger.debug(current_time)
+logger.debug('Constructing the log parser')
+log_parser = LogParser(log_format)
 
 logger.debug('Finding the log files:')
-log_files = glob.glob(log_dir + '/' + filename_pattern)
-logger.debug(log_files)
+log_file_paths = glob.glob(log_dir + '/' + filename_pattern)
+logger.debug(log_file_paths)
 
-logger.debug('Sorting by modified time and excluding old files')
-mtimes = {}
-for log_file in log_files:
-    mtimes[log_file] = os.path.getmtime(log_file)
+logger.debug('Parsing last timestamps in files and excluding old files.')
+last_timestamps = {}
+for log_file_path in log_file_paths:
+    # Open the log file so the last timestamp can be read.
+    with open(log_file_path, 'rb') as log_file:
+        try:
+            # Move to the second-to-last character in the file.
+            log_file.seek(-2, os.SEEK_END)
+            # Read the character, which seeks forward one character.
+            char = log_file.read(1)
+            # Until the character is a newline,
+            while char != b'\n':
+                # Seek back two characters.
+                log_file.seek(-2, os.SEEK_CUR)
+                # Read the character, which seeks forward one character.
+                char = log_file.read(1)
+        # An error will occur if the file is fewer than two lines long, in
+        # which case,
+        except OSError:
+            # Seek to the beginning of the file.
+            log_file.seek(0)
+        # Read the current line.
+        last_line = log_file.readline().decode('utf-8')
+    # Parse the line.
+    entry = log_parser.parse(last_line)
+    # Store the timestamp.
+    last_timestamps[log_file_path] = entry.request_time
     logger.debug(
-        'modified time of ' + log_file + ' is ' + str(mtimes[log_file])
+        'Last timestamp of ' + log_file_path + ' is '
+        + str(last_timestamps[log_file_path])
     )
-    if float(mtimes[log_file]) < float(last_run):
+    if last_timestamps[log_file_path] <= before_time:
         logger.debug(
-            'which is older than the last run, so will not be parsing it.'
+            'which is not after before_time, so will not be parsing it.'
         )
-        del mtimes[log_file]
-sorted_log_files = sorted(mtimes)
-logger.debug('Sorted list of log files:')
-logger.debug(sorted_log_files)
+        del last_timestamps[log_file_path]
+
+logger.debug('Sorting list of log files:')
+sorted_log_file_paths = sorted(last_timestamps)
+logger.debug(sorted_log_file_paths)
+
+logger.debug('Starting parsing of log files.')
+for log_file_path in sorted_log_file_paths:
+    logger.debug('Parsing ' + log_file_path)
+    with open(log_file_path, 'r') as log_file:
+        for line in log_file:
+            pass
 
 logger.debug('Writing values back to configuration file.')
-# Record the current time in the configuration file.
-conf.set('runs', 'last_run', str(current_time))
-
-# Write the configuration values back to the configuration file.
+#conf_parser.set('runs', 'last_run', str(current_time))
 comment_header = """\
 # This is a configuration file used by post_ood_logs_to_access_mms.py.
 #
@@ -134,6 +170,6 @@ comment_header = """\
 with open(args.conf_path, 'w') as conf_file:
     conf_file.write(comment_header)
 with open(args.conf_path, 'a') as conf_file:
-    conf.write(conf_file)
+    conf_parser.write(conf_file)
 
 logger.info('Script finished.')
