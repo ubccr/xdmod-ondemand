bc06d31b1c872cccc7dce842d0789c4e563ae84d
Aaron Weeden <aaronwee@buffalo.edu>
2024-11-14 13:12:33 -0500

    Fix 10.5.0 -> 11.0.1 bug.

--- configuration/etl/etl_sql.d/ood/ondemand-migration-11_0_0-11_0_1-fix-reverse-proxy-ports.sql
+++ configuration/etl/etl_sql.d/ood/ondemand-migration-11_0_0-11_0_1-fix-reverse-proxy-ports.sql
@@ -10,27 +10,51 @@
  * `page_impressions.reverse_proxy_port_id` < 65535 (otherwise 0), drop the
  * column `page_impressions.reverse_proxy_port_id`, and drop the table
  * `reverse_proxy_port`.
+ *
+ * This SQL is only run if the `reverse_proxy_port` column does not already
+ * exist. If it does exist, that means XDMoD is being upgraded from 10.5.0 to
+ * 11.0.1 and the column was created in the migration from 10.5.0 to 11.0.0,
+ * which means the `reverse_proxy_port_id` column doesn't yet have any
+ * data, in which case the remaining SQL is unnecessary.
  */
-ALTER TABLE
-    ${DESTINATION_SCHEMA}.page_impressions
-ADD
-    reverse_proxy_port smallint(5) unsigned NOT NULL
-//
-UPDATE
-    ${DESTINATION_SCHEMA}.page_impressions p
-LEFT JOIN
-    ${DESTINATION_SCHEMA}.reverse_proxy_port rpp ON rpp.id = p.reverse_proxy_port_id
-SET
-    reverse_proxy_port = IF(
-        reverse_proxy_port_id < 65535,
-        COALESCE(rpp.port, 0),
-        0
-    )
-//
-ALTER TABLE
-    ${DESTINATION_SCHEMA}.page_impressions
-DROP COLUMN
-    reverse_proxy_port_id
-//
-DROP TABLE reverse_proxy_port
-//
+
+DELIMITER $$
+IF NOT EXISTS(
+    SELECT
+        1
+    FROM
+        INFORMATION_SCHEMA.COLUMNS
+    WHERE
+        TABLE_SCHEMA = '${DESTINATION_SCHEMA}'
+    AND
+        TABLE_NAME = 'page_impressions'
+    AND
+        COLUMN_NAME = 'reverse_proxy_port'
+)
+BEGIN
+    ALTER TABLE
+        ${DESTINATION_SCHEMA}.page_impressions
+    ADD
+        reverse_proxy_port smallint(5) unsigned NOT NULL
+    //
+    UPDATE
+        ${DESTINATION_SCHEMA}.page_impressions p
+    LEFT JOIN
+        ${DESTINATION_SCHEMA}.reverse_proxy_port rpp ON rpp.id = p.reverse_proxy_port_id
+    SET
+        reverse_proxy_port = IF(
+            reverse_proxy_port_id < 65535,
+            COALESCE(rpp.port, 0),
+            0
+        )
+    //
+    ALTER TABLE
+        ${DESTINATION_SCHEMA}.page_impressions
+    DROP COLUMN
+        reverse_proxy_port_id
+    //
+    DROP TABLE reverse_proxy_port
+    //
+END
+$$
+DELIMITER //
