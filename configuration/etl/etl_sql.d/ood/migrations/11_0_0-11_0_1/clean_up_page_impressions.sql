4d0962176ad1a290e892372089da76103bb74b3e
Aaron Weeden <aaronwee@buffalo.edu>
2025-02-26 12:55:01 -0500

    Refactor.

---
+++ configuration/etl/etl_sql.d/ood/migrations/11_0_0-11_0_1/clean_up_page_impressions.sql
@@ -0,0 +1,6 @@
+DROP TABLE ${DESTINATION_SCHEMA}.page_impressions
+//
+RENAME TABLE ${DESTINATION_SCHEMA}.11_0_0_11_0_1_new_page_impressions TO ${DESTINATION_SCHEMA}.page_impressions
+//
+DROP TABLE ${DESTINATION_SCHEMA}.reverse_proxy_port
+//
